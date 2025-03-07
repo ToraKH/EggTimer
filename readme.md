3765d5b0bb05bd9473013868be5d5719482932b3
Tora <tohom2986@uit.no>
2025-03-07 22:51:45 +0100

    Implemented egg timer

---
+++ readme.md
@@ -0,0 +1,28 @@
+# Egg timer
+This is a timer for how long you should boil you eggs based on if you want them hard, soft or runny. 
+
+## Author
+Tora K. Homme
+
+## How to run code
+### Using link
+Use the link here to access it on any device:
+
+### Locally
+Navigate to the folder and double-click on the index.html file. This will open up the timer on your browser.
+
+## Instructions
+### How to start/pause/change the timer
+You simply need to select default, hard, soft or runny, and then press start. If you want to change boil type you can easily do so by clicking the prefered button. If you need to pause, you can do so by pressing the pause button. To start the timer, you always need to press start after stopping, even when changing boil type.
+
+### Alarm sound
+When the time is up, you will hear an awsome alarm sound. However, after some time it might become somewhat annoying. To turn of the alarm, simply press the pause button!
+
+
+
+## Credits
+The timer function is inspired by this tutorial by Florin Pop (2020, jan 21): 
+https://www.youtube.com/watch?v=x7WJEmxNlEs.
+
+The rooster audio is from mixkit: https://mixkit.co/free-sound-effects/alarm/. The audio is called "Rooster crowing in the morning".
+
---
+++ script.js
@@ -0,0 +1,66 @@
+
+var startingMinutes = 8.5; // default is 8.5 because it is epic
+var paused = 0;
+var time = startingMinutes * 60;
+
+
+const chooseTime = document.getElementById('chosenTime');
+chooseTime.innerHTML = 'Select boil type';
+
+
+document.addEventListener('click', function (event){
+    let type = event.target.getAttribute('data-click');
+    if(!type) return;
+
+    if (type == 'HardBoiled'){
+        var startingMinutes = 10;
+        chooseTime.innerHTML = 'You chose 10 minutes';
+    }
+    if(type == 'SoftBoiled'){
+        var startingMinutes = 8;
+        chooseTime.innerHTML = 'You chose 8 minutes';
+    }
+    if(type == 'Runny'){
+        var startingMinutes = 6;
+        chooseTime.innerHTML = 'You chose 6 minutes';
+    }
+    if(type == 'Default'){
+        var startingMinutes = 0.1;
+        chooseTime.innerHTML = 'You chose default time: 8½ minutes';
+    }
+    time = (startingMinutes || 0) * 60; //bruk 0 hvis ikke minutter er valgt
+    
+});
+
+
+const countdownEl = document.getElementById('countdown');
+const finished = document.getElementById('done');
+finished.innerHTML.display="none";
+var interval;
+
+
+function start(){
+    interval = setInterval(updateCountdown, 1000);
+    document.getElementById("startbtn").disabled = true;
+    document.getElementById("stopbtn").disabled = false;
+
+}
+function stop(){
+    clearInterval(interval);
+    document.getElementById("startbtn").disabled = false;
+    document.getElementById("stopbtn").disabled = true;
+}
+
+function updateCountdown(){
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds; // get :01 instead of just :1
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+    time = time < 0 ? 0:time; // set time to 0 so it does not get negative time
+    if(time == 0){
+        document.getElementById('endOfTimer').play();
+    }
+}
+
